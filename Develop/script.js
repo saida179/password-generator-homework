cb638a5b2e40f8b9713a437042552750ad24c03d
saida179 <69127945+saida179@users.noreply.github.com>
2020-08-29 10:07:26 -0700

    adjusting code for homework password generator

--- Develop/script.js
+++ Develop/script.js
@@ -19,14 +19,22 @@ function writePassword() {
 function generatePassword(){
 
   let passwordLength = prompt("Enter a password that is the length of at least 8 characters and no more than 128 characters!");
-
-  let passwordUperCase = confirm(" Do you want to use upcase?")
-
+  let passwordUpperCase = confirm(" Do you want to use uppercase?")
   let passwordLowerCase = confirm(" Do you want to use lowercase?")
   let passwordNumbers = confirm(" Do you want to use numbers?")
   let passwordSpecialCharacters = confirm(" Do you want to use specail characters?")
 
-  console.log(passwordLength, passwordLowerCase, passwordNumbers, passwordSpecialCharacters, passwordUperCase)
+  console.log(passwordLength, passwordLowerCase, passwordNumbers, passwordSpecialCharacters, passwordUpperCase)
+
+  const alphabetUpperCase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  const alphabetLowerCase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+  const numbers=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  const specialCharacters=
 
 
   // Password is currently blank! We need to make a better one
